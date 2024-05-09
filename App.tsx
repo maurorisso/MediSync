d0a2a7b265dc51d5d43f7ef3075c83071729421a
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2024-05-09 13:14:27 +0200

    feat(search): add optimized patient search using useMemo

--- App.tsx
+++ App.tsx
@@ -8,23 +8,42 @@ import {
   ActivityIndicator,
   SafeAreaView,
   FlatList,
+  TextInput,
+  ScrollView,
 } from "react-native";
 
-import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { Patient } from "./types/patientTypes";
+import { useMemo, useState } from "react";
 import PatientItem from "./components/PatientItem";
-import patients from "./data/patients";
+import mockList from "./data/patients";
 
 const App = () => {
+  const [searchText, setSearchText] = useState<string>("");
+
+  const filteredPatients = useMemo(() => {
+    return mockList.filter((patient) =>
+      patient.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }, [searchText]);
+
   return (
-    <SafeAreaView className=" bg-blue-50  items-center justify-center   ">
-      <FlatList
-        data={patients}
-        keyExtractor={(item) => item.id.toString()}
-        className="w-full px-4 flex-col  "
-        renderItem={({ item }) => <PatientItem item={item} />}
-      />
+    <SafeAreaView className=" bg-blue-50  items-center justify-center">
+      <ScrollView className="w-full">
+        <Text className="text-2xl font-bold text-center p-4">MediSync</Text>
+        <TextInput
+          placeholder="Search..."
+          value={searchText}
+          onChangeText={(text) => {
+            setSearchText(text);
+          }}
+          className="w-ful m-auto  text-black w-3/4 p-2 rounded-lg bg-white"
+        />
+        <FlatList
+          data={filteredPatients}
+          keyExtractor={(item) => item.id.toString()}
+          className="w-full px-4 flex-col  "
+          renderItem={({ item }) => <PatientItem item={item} />}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- components/PatientItem.tsx
+++ components/PatientItem.tsx
@@ -13,26 +13,32 @@ const PatientItem = ({ item }: Props) => {
       : item.status === "stable"
       ? "green"
       : "yellow";
+  const title = item.gender === "male" ? "Mr." : "Ms.";
 
   return (
-    <View className="w-full bg-white  rounded-lg   p-4 justify-between items-center  flex-row mt-4 ">
+    <View className="w-full bg-white  rounded-lg   p-2 justify-between items-center  flex-row mt-4 ">
       <Image
         source={{ uri: item.picture }}
-        className="h-20 w-20 rounded-2xl "
+        className="h-20 w-20 rounded-full "
         resizeMode="cover"
       />
-      <View className="text-left  flex-1  p-4 here">
-        <Text className="text-xl  font-bold text-gray-900">{item.name}</Text>
+      <View className="flex-1  p-2 here">
+        <View className="flex flex-row  items-center  ">
+          <Text className=" text-xl "> {title}</Text>
+          <Text className=" ml-1  text-xl  font-bold text-gray-900">
+            {item.name}
+          </Text>
+        </View>
         <Text className="text-lg text-gray-800">{item.diagnosis}</Text>
-        <View className="flex flex-row  justify-between ">
-          <View className={`flex flex-row items-center justify-center`}>
+        <View className={`flex flex-row  justify-between`}>
+          <View className={`flex flex-row items-center justify-center  w-fit`}>
             <View
               className={`h-2.5 w-2.5 rounded-full  bg-${status}-400 mr-1 `}
             ></View>
             <Text className={`text-base capitalize`}>{item.status} </Text>
           </View>
           <View className=" bg-gray-50  font-base  px-2 py-1 rounded-md   ">
-            <Text>{item.bedNumber}</Text>
+            <Text> Room {item.bedNumber}</Text>
           </View>
         </View>
       </View>
