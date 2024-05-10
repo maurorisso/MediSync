20d115d1b8bc9c5dbd1511f1c5163efd06583097
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2024-05-10 12:03:43 +0200

    fix(android): Update Header component to handle platform-specific margin

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import React from "react";
 import { Hospital } from "lucide-react-native";
 
@@ -6,7 +6,12 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <View className="flex     justify-center flex-row items-center pt-4 mb-5   gap-1">
+    <View
+      style={{
+        marginTop: Platform.OS === "android" ? 20 : 0,
+      }}
+      className="flex     justify-center flex-row items-center pt-4 mb-5 gap-1"
+    >
       <Hospital size={32} className=" text-blue-800  " />
       <Text className="text-2xl font-semibold   ">MediSync</Text>
     </View>
